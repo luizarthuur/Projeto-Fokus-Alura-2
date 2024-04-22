3cb33771ca5ef92d4689a792217a10537a5771a0
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-04-22 20:35:18 -0300

    continuação do projeto

--- exercicios.js
+++ exercicios.js
@@ -14,3 +14,53 @@ formulário.addEventListener('submit', () => {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
 
+botao.onclick = () => {
+    //debugger linha utilizada para debugar o que queremos criar, podendo passar linha a linha para entender o problema.
+    const novaDescricao = prompt('Qual é o novo nome da tarefa?');
+    if (novaDescricao) {
+        paragrafo.textContent = novaDescricao
+        tarefa.descricao = novaDescricao
+        atualizarTarefas();        
+    }
+
+}
+
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
+elemento-lista.onclick = () => { //ao clicar em um elemento da lista, faça
+    document.querySelectorAll('.classe-ativo') //selecionando todos os elementos que tem a classe ativo, ou seja, que tem um destaque por estarem selecionados 
+    paragrafoEmAndamento.textArea = descricaoDaTarefa
+    .forEach(elemento => { //Removendo a classe ativa de todos os itens
+        elemento.classList.remove('app__section-task-list-item-active')
+    })
+    if (tarefaSelecionada == tarefa) { //verificando se a tarefa selecionada foi clicada novamente, caso sim, ele remove o texto da LI e também a classe que destaca o elemento
+        paragrafoDescricaoTarefa.textContent = ''
+        tarefaSelecionada = null
+        liTarefaSelecionada = null
+        return
+    }
+
+    tarefaSelecionada = tarefa //tornando a tarefaSelecionada como a tarefa clicada (de acordo com o clique)
+    liTarefaSelecionada = li //tornando a li Selecionada como a li clicada clicada (de acordo com o clique)
+    paragrafoDescricaoTarefa.textContent = tarefa.descricao // tornando o parágrafo de acordo com a tarefa em questão       
+
+
+}
+
+li.classList.add('app__section-task-list-item-active'); // adicionando a classe ativa para o que está clicado
+
+let eventoAtivo = html.getAttribute('data-contexto') == 'foco'
+if (eventoAtivo) {
+    const evento = new CustomEvent('focoFinalizado');
+    document.dispatchEvent(evento); 
+}
+
+document.addEventListener('focoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+    }
+})
--- fokus-local-storage-main/fokus-local-storage-main/script-crud.js
+++ fokus-local-storage-main/fokus-local-storage-main/script-crud.js
@@ -3,30 +3,70 @@ const form = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list')
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+const botaoCancelar = document.querySelector('.app__form-footer__button--cancel');
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
+let tarefaSelecionada = null
+let liTarefaSelecionada = null
 
 btnAdicionarTarefa.addEventListener('click', () => {
     form.classList.toggle('hidden')
 })
 
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
 function criarElementoTarefa (tarefa) {
     const li = document.createElement('li');
-    li.classList.add('.app__section-task-list-item')
+    li.classList.add('app__section-task-list-item')
     
     const svg = document.createElement('svg');
-    svg.innerHTML = `        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
-    xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-    <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
-        fill="#01080E"></path>
-    </svg>`
+    svg.innerHTML = `<svg class="app__section-task-icon-status" width="24"      height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
+        </svg>`
 
     const paragrafo = document.createElement('p');
     paragrafo.textContent = tarefa.descricao
+    paragrafo.classList.add('app__section-task-list-item-description')
 
     const botao = document.createElement('button');
     const imagemDoBotao = document.createElement('img');
-
+    botao.classList.add('app_button-edit')
     imagemDoBotao.setAttribute('src', 'imagens/edit.png');
+    botao.append(imagemDoBotao)
+
+    botao.onclick = () => {
+        //debugger linha utilizada para debugar o que queremos criar, podendo passar linha a linha para entender o problema.
+        const novaDescricao = prompt('Qual é o novo nome da tarefa?');
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao
+            tarefa.descricao = novaDescricao
+            atualizarTarefas();        
+        }
+
+    }
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+        .forEach(elemento => {
+            elemento.classList.remove('app__section-task-list-item-active')
+        })
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            liTarefaSelecionada = null
+            return
+        }
+        tarefaSelecionada = tarefa
+        liTarefaSelecionada = li
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao       
+
+        li.classList.add('app__section-task-list-item-active');
+        
+    }
 
     li.append(svg)
     li.append(paragrafo)
@@ -42,10 +82,27 @@ form.addEventListener('submit', (evento) => {
         descricao: textArea.value
     }
     tarefas.push(tarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+    atualizarTarefas();
+    textArea.value = ''
+    form.classList.add('hidden')
 })
 
 tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
+})
+
+botaoCancelar.addEventListener('click', () =>{
+    textArea.value = '';
+    form.classList.toggle('hidden');
+})
+
+document.addEventListener('focoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+    }
 })
